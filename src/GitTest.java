35fba50cb9c96db51452b654e47ce490c7efb9b3
Ireneusz Piątkowski <irek.piatkowski@gmail.com>
2019-11-16 18:55:22 +0100

    Zmiany dodane przez dev1

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Nasz pierwszy program");
         System.out.println("Zmiana dodana przez dev2");
+        System.out.println("A to zmiany dodane przez dev1");
     }
 }
