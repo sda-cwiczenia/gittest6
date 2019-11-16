a2e846491cd271c391b5f64da998fe4fa8949250
Ireneusz Piątkowski <irek.piatkowski@gmail.com>
2019-11-16 21:25:34 +0100

    Zmiana dodana w gałęzi test

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,7 @@ public class GitTest {
         System.out.println("Nasz pierwszy program");
         System.out.println("Zmiana dodana przez dev2");
         System.out.println("A to zmiany dodane przez dev1");
-        System.out.println("Nowa zmiana dodana mo operacji merge");    }
+        System.out.println("Nowa zmiana dodana mo operacji merge");
+        System.out.println("Zmiany dodane w gałęzi test");
+    }
 }
